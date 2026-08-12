ec870424448994a180a536e7180c383d9a3cbf13
SatyamKumar457 <satyamkumar1092004@gmail.com>
2026-08-12 23:54:09 +0530

    Second Version.

--- main.py
+++ main.py
@@ -23,3 +23,14 @@ def view():
     data = load_data()
 
     return data
+
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id:str):
+    # load all the patients
+    data = load_data()
+
+
+    if patient_id in data:
+        return data[patient_id]
+    else:
+        return {'error':'patient not found.'}
